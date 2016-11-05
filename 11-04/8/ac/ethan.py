e889502fad33ab2f398b0837a7d4830c13833126
Ethan Arnold <masonsbro456@gmail.com>
2016-11-05 16:32:27 -0500

    Change Ethan's 8 solution

--- 11-04/8/ac/ethan.py
+++ 11-04/8/ac/ethan.py
@@ -2,9 +2,12 @@ def bf(edges, most, reach, N):
     for rnd in range(N - 1):
         for u, v, w in edges:
             if reach[u]:
-                reach[v] = True
-                most[v] = max(most[v], most[u] + w)
-    return most[N]
+                if not reach[v]:
+                    reach[v] = True
+                    most[v] = most[u] + w
+                else:
+                    most[v] = max(most[v], most[u] + w)
+    return most[N] if N in most else 0
 
 T = int(raw_input())
 
@@ -14,8 +17,9 @@ for t in range(T):
     for m in range(M):
         U, V, W = map(int, raw_input().split())
         edges.append((U, V, W))
-    most = {i: 0 for i in range(1, N + 1)}
+    most = {}
     reach = {i: False for i in range(1, N + 1)}
+    most[1] = 0
     reach[1] = True
     first = bf(edges, most, reach, N)
     second = bf(edges, most, reach, N)
